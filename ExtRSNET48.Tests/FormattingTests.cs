5c4ebaee62579e561090fd8772bb843a34b43ad2
colin fitzgerald <cfitzg1983@gmail.com>
2023-05-10 20:20:59 -0500

    Remove Oracle from regex, it's drunk

--- ExtRSNET48.Tests/FormattingTests.cs
+++ ExtRSNET48.Tests/FormattingTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Sonrai.ExtRSNET48.UnitTests
@@ -65,6 +66,318 @@ namespace Sonrai.ExtRSNET48.UnitTests
         {
             var match = Regex.Match("11-2-34453-1", FormattingService.SocialSecurity);
             Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void AlphanumericSucceeds()
+        {
+            var match = Regex.Match("AaaBbbCcc1133", FormattingService.Alphanumeric);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void AlphanumericWithSpacesSucceeds()
+        {
+            var match = Regex.Match("Aaa BbbCcc111222333", FormattingService.AlphanumericWithSpaces);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void AlphanumericWithSpacesFails()
+        {
+            var match = Regex.Match("Aaa^BbbCcc+111222333", FormattingService.AlphanumericWithSpaces);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void CardAmexSucceeds()
+        {
+            var match = Regex.Match("378282246310005", FormattingService.CardAmex);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void CardAmexFails()
+        {
+            var match = Regex.Match("3782224631005", FormattingService.CardAmex);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void CardDiscoverSucceeds()
+        {
+            var match = Regex.Match("6011000990139424", FormattingService.CardDiscover);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void CardDiscoverFails()
+        {
+            var match = Regex.Match("601111111111117", FormattingService.CardDiscover);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void CardMastercardSucceeds()
+        {
+            var match = Regex.Match("5555555555554444", FormattingService.CardMastercard);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void CardMastercardFails()
+        {
+            var match = Regex.Match("55555555555544", FormattingService.CardMastercard);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void ComplexPasswordSucceeds()
+        {
+            var match = Regex.Match("lL1!eightchars", FormattingService.ComplexPassword);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void ComplexPasswordFails()
+        {
+            var match = Regex.Match("lL1eightchars", FormattingService.ComplexPassword);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void DecimalNumbersSucceeds()
+        {
+            var match = Regex.Match("3.14", FormattingService.DecimalNumbers);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void DecimalNumbersFails()
+        {
+            var match = Regex.Match("5", FormattingService.DecimalNumbers);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void DateWithSlashesSucceeds()
+        {
+            var match = Regex.Match("03/15/1983", FormattingService.DateWithSlashes);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void DateWithSlashesFails()
+        {
+            var match = Regex.Match("15/3-1983", FormattingService.DateWithSlashes);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void AmericanEnglishDateSucceeds()
+        {
+            var match = Regex.Match("03/15/1983", FormattingService.AmericanEnglishDate);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void AmericanEnglishDateFails()
+        {
+            var match = Regex.Match("15/03/1983", FormattingService.AmericanEnglishDate);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void BritishEnglishDateSucceeds()
+        {
+            var match = Regex.Match("15/03/1983", FormattingService.BritishEnglishDate);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void BritishEnglishDateFails()
+        {
+            var match = Regex.Match("15/14/1983", FormattingService.BritishEnglishDate);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void FilePathSucceeds()
+        {
+            var match = Regex.Match("var/some/place.pdf", FormattingService.FilePath);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void FilePathFails()
+        {
+            var match = Regex.Match("var|some|place.", FormattingService.FilePath);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void HasDupelicatesSucceeds()
+        {
+            var match = Regex.Match("this contains contains dupes", FormattingService.HasDupelicates);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void HasDupelicatesFails()
+        {
+            var match = Regex.Match("eachcharisdiferent eachWordIsDifferent", FormattingService.HasDupelicates);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void HtmlTagSucceeds()
+        {
+            var match = Regex.Match("<input type='button' />", FormattingService.HtmlTag);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void HtmlTagFails()
+        {
+            var match = Regex.Match("{'some':'value' }", FormattingService.HtmlTag);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void HttpsUrlSucceeds()
+        {
+            var match = Regex.Match("https://tel.net", FormattingService.HttpsUrl);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void HttpsUrlFails()
+        {
+            var match = Regex.Match("http//tel.net", FormattingService.HttpsUrl);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void IpV4Succeeds()
+        {
+            var match = Regex.Match("8.8.8.8", FormattingService.IpV4);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void IpV4Fails()
+        {
+            var match = Regex.Match("8.7.5.esVV.oc.oc", FormattingService.IpV4);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void IpV6Succeeds()
+        {
+            var match = Regex.Match("2001:0db8:85a3:0000:0000:8a2e:0370:7334", FormattingService.IpV6);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void IpV6Fails()
+        {
+            var match = Regex.Match("1:22:85a3:0000-27-8a2e-0370-7334", FormattingService.IpV6);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void IpV4AndIpV6Succeeds()
+        {
+            var match = Regex.Match("8.8.8.4", FormattingService.IpV4AndIpV6);
+            Assert.IsTrue(match.Success);
+            match = Regex.Match("2001:0db8:85a3:0000:0000:8a2e:0370:7334", FormattingService.IpV4AndIpV6);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void IIpV4AndIpV6Fails()
+        {
+            var match = Regex.Match("1:33:85a3:0000-27--0370-_", FormattingService.IpV4AndIpV6);
+            Assert.IsTrue(!match.Success);
+            match = Regex.Match("89.345.23.aa.c-1", FormattingService.IpV4AndIpV6);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void IsoDashDateSucceeds()
+        {
+            var match = Regex.Match("2024-01-01", FormattingService.IsoDashDate);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void IsoDashDateFails()
+        {
+            var match = Regex.Match("00/11/234", FormattingService.IsoDashDate);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void ModeratePasswordSucceeds()
+        {
+            var match = Regex.Match("l34534Rdhfbdth", FormattingService.ModeratePassword);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void ModeratePasswordFails()
+        {
+            var match = Regex.Match("lRdhdth", FormattingService.ModeratePassword);
+            Assert.IsTrue(!match.Success);
+        }
+
+        [TestMethod]
+        public void OracleDateSucceeds()
+        {
+            var match = Regex.Match("01-APR-1998", FormattingService.OracleDate);
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void OracleDateFails()
+        {
+            var match = Regex.Match("JAN-01-2024", FormattingService.OracleDate);
+            Assert.IsTrue(!match.Success);
+        }
+
+
+
+        [TestMethod]
+        public void DelimitedToJsonSucceeds()
+        {
+            string csv = @"comma,separated,values
+                        are,super,duper
+                        fun,fun,forever
+                        data, is, beautiful";
+            string json = FormattingService.CsvToJson(csv, ",");
+            Assert.IsTrue(JToken.Parse(json).HasValues);
+        }
+
+        [TestMethod]
+        public void ConvertJsonToXmlSucceeds()
+        {
+            string result = FormattingService.ConvertJsonToXml("{ 'some':'thing' }");
+            Assert.IsTrue(result.Contains("/Root>"));
+        }
+
+        [TestMethod]
+        public void ConvertXmlToJsonSucceeds()
+        {
+            string result = FormattingService.ConvertXmlToJson("<xml><childXml></childXml></xml>");
+            Assert.IsTrue(result.Contains("{"));
+        }
+
+        [TestMethod]
+        public void SerializeObjectSucceeds()
+        {
+            List<string> list = new List<string> { "Aa", "Bb", "Cc" };
+            var result = FormattingService.SerializeObject(list);
+            Assert.IsTrue(result.Contains("["));
         }
     }
 }
--- ExtRSNET48/Services/FormattingService.cs
+++ ExtRSNET48/Services/FormattingService.cs
@@ -45,11 +45,6 @@ namespace Sonrai.ExtRSNET48
             return Convert.ToDecimal(value) / 1000000000000;
         }
 
-        public static string DelimitedToJson(string json)
-        {
-            return JsonConvert.DeserializeXmlNode(json).ToString();
-        }
-
         public static string ConvertXmlToJson(string xml)
         {
             XmlDocument doc = new XmlDocument();
@@ -63,7 +58,7 @@ namespace Sonrai.ExtRSNET48
             return JsonConvert.DeserializeXNode(json, "Root").ToString();
         }
 
-        public string SerializeObject(object obj)
+        public static string SerializeObject(object obj)
         {
             return JsonConvert.SerializeObject(obj);
         }
@@ -112,70 +107,74 @@ namespace Sonrai.ExtRSNET48
         #region CommonRegex
         //ref: //https://digitalfortress.tech/tips/top-15-commonly-used-regex/ 
 
-        public static string WholeNumbers = "/^\\d+$/";
+        public static string WholeNumbers = "^\\d+$";
 
-        public static string DecimalNumbers = "/^\\d*\\.\\d+$/";
+        public static string DecimalNumbers = "^\\d*\\.\\d+$";
 
-        public static string WholeAndDecimalNumbers = "/^\\d*(\\.\\d+)?$/";
+        public static string WholeAndDecimalNumbers = "^\\d*(\\.\\d+)?$";
 
-        public static string GetShrugText = "¯\\_(ツ)_/¯";
+        public static string ShrugText = "¯\\_(ツ)_/¯";
 
-        public static string Alphanumeric = "/^[a-zA-Z0-9]*$/";
+        public static string Alphanumeric = "^[a-zA-Z][a-zA-Z0-9]*$";
 
-        public static string AlphanumericWithSpaces = "/^[a-zA-Z0-9 ]*$/";
+        public static string AlphanumericWithSpaces = "^[a-zA-Z][a-zA-Z0-9 ]*$";
 
         // Should have 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character and be at least 8 characters long
-        public static string ComlpexPassword = @"/(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;""'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/";
+        public static string ComplexPassword = @"(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;""'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
 
         // Should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long
-        public static string ModeratePassword = "/(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/";
+        public static string ModeratePassword = "(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$";
 
         // Alphanumeric string that may include _ and – having a length of 3 to 16 characters
-        public static string HttpsUrl = "/https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#()?&//=]*)/";
+        public static string HttpsUrl = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#()?&//=]*)";
 
-        public static string Uri = "/(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/ ";
+        public static string Uri = "(https?:\\/\\/)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
 
-        public static string IpV4 = "/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/";
+        public static string IpV4 = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
 
-        public static string IpV6 = @"/(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/";
+        public static string IpV6 = @"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
 
-        public static string UserName = "/^[a-z0-9_-]{3,16}$/";
+        public static string UserName = "^[a-z0-9_-]{3,16}$";
 
-        public static string IpV4AndIpV6 = @"/((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/";
+        public static string IpV4AndIpV6 = @"((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))";
 
         /* Date Format YYYY-MM-dd */
-        public static string IsoDashDate = "/([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))/";
+        public static string IsoDashDate = "^(\\d{4,5}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]))$";
 
-        public static string EnglishDate = "/^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$/";
+        public static string DateWithSlashes = "^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$";
 
-        public static string OracleDate = "/^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)(?:0?2|(?:Feb))\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$/";
+        public static string BritishEnglishDate = "(0?[1-9]|[12][0-9]|3[01])[- /.](0?[1-9]|1[012])[- /.](19|20)\\d\\d";
 
-        public static string Time12Hour = "/((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))/";
+        public static string AmericanEnglishDate = "(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)[0-9]{2}";
 
-        public static string Time24Hour = "/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/";
+        public static string SqlServerDate = "^[\\w]{3} {1,2}[\\d]{1,2} [\\d]{4} ([\\d]{2}:){3}[\\d]{3}(AM|PM)$";
 
-        public static string HtmlTag = "/<\\/?[\\w\\s]*>|<.+[\\W]>/";
+        public static string OracleDate = "d{2}-[a-zA-Z]{3}-d{4}";
 
-        public static string HasDupelicates = "/(\\b\\w+\\b)(?=.*\\b\\1\\b)/";
+        public static string Time12Hour = "((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))";
 
-        public static string PhoneNumberInternational = "/^(?:(?:\\(?(?:00|\\+)([1-4]\\d\\d|[1-9]\\d?)\\)?)?[\\-\\.\\ \\\\\\/]?)?((?:\\(?\\d{1,}\\)?[\\-\\.\\ \\\\\\/]?){0,})(?:[\\-\\.\\ \\\\\\/]?(?:#|ext\\.?|extension|x)[\\-\\.\\ \\\\\\/]?(\\d+))?$/";
+        public static string Time24Hour = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
 
-        public static string FilePathWithExt = "/((\\/|\\\\|\\/\\/|https?:\\\\\\\\|https?:\\/\\/)[a-z0-9 _@\\-^!#$%&+={}.\\/\\\\\\[\\]]+)+\\.[a-z]+$/";
+        public static string HtmlTag = "<\\/?[\\w\\s]*>|<.+[\\W]>";
 
-        public static string FilePathWithoutExt = "/^(.+)/([^/]+)$/";
+        public static string HasDupelicates = "(\\b\\w+\\b)(?=.*\\b\\1\\b)";
+
+        public static string PhoneNumberInternational = "^(?:(?:\\(?(?:00|\\+)([1-4]\\d\\d|[1-9]\\d?)\\)?)?[\\-\\.\\ \\\\\\/]?)?((?:\\(?\\d{1,}\\)?[\\-\\.\\ \\\\\\/]?){0,})(?:[\\-\\.\\ \\\\\\/]?(?:#|ext\\.?|extension|x)[\\-\\.\\ \\\\\\/]?(\\d+))?$";
+
+        public static string FilePath = "^(.+)\\/([^\\/]+)$";
 
         public static string UsPostalCode = "^(?!0{3})[0-9]{3,5}$";
 
         public static string SocialSecurity = "^((?!219-09-9999|078-05-1120)(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4})|((?!219 09 9999|078 05 1120)(?!666|000|9\\d{2})\\d{3} (?!00)\\d{2} (?!0{4})\\d{4})|((?!219099999|078051120)(?!666|000|9\\d{2})\\d{3}(?!00)\\d{2}(?!0{4})\\d{4})$";
 
-        public static string Passport = "/^[A-PR-WY][1-9]\\d\\s?\\d{4}[1-9]$/";
+        public static string Passport = "^[A-PR-WY][1-9]\\d\\s?\\d{4}[1-9]$";
 
         // can use either hypen(-) or space( ) character as separator
         // ref: https://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
 
         public static string CardAmex = "^3[47][0-9]{13}$";
 
-        public static string CardDiscover = "^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9]";
+        public static string CardDiscover = "^6(?:011\\d{12}|5\\d{14}|4[4-9]\\d{13}|22(?:1(?:2[6-9]|[3-9]\\d)|[2-8]\\d{2}|9(?:[01]\\d|2[0-5]))\\d{10})$";
 
         public static string CardMastercard = "^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$";
 
