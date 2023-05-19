1c1fa267b6b2e39ed83cdd246f6ea9311f64bc2b
colin fitzgerald <colin@sonrai.io>
2023-05-19 18:26:44 -0500

    Adding SSRS models

--- ExtRSNET48/Models/SSRSModels.cs
+++ ExtRSNET48/Models/SSRSModels.cs
@@ -1,20 +1,79 @@
-﻿namespace Sonrai.ExtRS.Models
-{
-    public class SSRSModel
-    {
+﻿using Newtonsoft.Json;
 
+namespace Sonrai.ExtRS.Models
+{
+    public class SSRSConnection
+    {
+        private string sqlAuthCookie;
         public string Administrator = "ExtRSAuth";
         public string UserName;
+        public string UserRole;
         public string ServerUrl;
-        public string ClientBearerToken;
+        public AuthenticationType AuthenticationType;
+
         protected bool IsOnline = false;
 
-        public SSRSModel(string serverUrl, string adminUser, string token = "") 
+        public SSRSConnection(string serverUrl, string adminUser, string cookie, AuthenticationType authType = AuthenticationType.ExtRSAuth)
         {
             ServerUrl = serverUrl;
-            Administrator = adminUser ?? Administrator;         
-            ClientBearerToken = token ?? ClientBearerToken;
+            Administrator = adminUser ?? Administrator;
+            sqlAuthCookie = cookie;
+            AuthenticationType = authType;
         }
+    }
+
+    public abstract class CatalogItem
+    {
+        [JsonProperty("@odata.type")]
+        string ODataType;
+        string Id;
+        string Name;
+        string Description;
+        string Path;
+        string Type;
+        bool Hidden;
+        int Size;
+        string ModifiedBy;
+        string ModifiedDate;
+        string CreatedBy;
+        string CreatedDate;
+        string ParentFolderId;
+        bool IsFavorite;
+        string ContentType;
+        string Content;
+        string[] Roles;
+    }
+
+    public class Report : CatalogItem
+    {
+        bool HasDataSources;
+        bool HasSharedDataSets;
+        bool HasParameters;
+    }
+
+    public class DataSet
+    {
+        bool QueryExecutionTimeOut;
+        bool HasParameters;
+    }
+
+    public class Folder : CatalogItem
+    {
+
+    }
+
+    public class DataSource : CatalogItem
+    {
+        bool IsEnabled;
+        string ConnectionString;
+        string DataSourceType;
+        bool IsOriginalConnectionStringExpressionBased;
+        bool IsConnectionStringOverridden;
+        string CredentialRetrieval;
+        bool IsReference;
+        string DataSourceSubType;
+        string CredentialsByUser;
+        string CredentialsInServer;
     }
 
     public enum AuthenticationType
